37609f50ac8af62c1e3c93c569bc2a4daa823e6f
SBC4 <SBC4@SBC08>
2015-04-28 14:13:45 +0700

    belajarpenjumlahan

---
+++ src/dmj_klompok5/dita.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dmj_klompok5;
+
+/**
+ *
+ * @author SBC4
+ */
+public class dita {
+    public static void main(String[] args) {
+        
+    
+       int a=7;
+       int b=8;
+   int c=a*b;
+    System.out.println(c);
+}}
